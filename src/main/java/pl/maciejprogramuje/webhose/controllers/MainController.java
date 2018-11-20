1e6f2a06dabfabce8410c2047f92bbc0ec2a4f3b
maciejprogramuje <maciejprogramuje@gmail.com>
2018-11-20 12:44:14 +0100

    ok

--- src/main/java/pl/maciejprogramuje/webhose/controllers/MainController.java
+++ src/main/java/pl/maciejprogramuje/webhose/controllers/MainController.java
@@ -1,10 +1,14 @@
 package pl.maciejprogramuje.webhose.controllers;
 
+import com.google.gson.JsonElement;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import pl.maciejprogramuje.webhose.WebhoseIOClient;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,11 +25,18 @@ public class MainController {
         queryButton.setOnAction(event -> {
             System.out.println("click!");
 
-            /*WebhoseIOClient webhoseClient = WebhoseIOClient.getInstance("5e4f2b3f-7b67-4646-b8ca-a661bc1be8c0");
+            WebhoseIOClient webhoseClient = WebhoseIOClient.getInstance("5e4f2b3f-7b67-4646-b8ca-a661bc1be8c0");
 
             Map<String, String> queries = new HashMap<String, String>();
             queries.put("q", "thread.section_title:instalki language:polish");
-            queries.put("sort", "crawled");*/
+            queries.put("sort", "crawled");
+
+            try {
+                JsonElement result = webhoseClient.query("filterWebContent", queries);
+                System.out.println(result.getAsJsonObject().get("totalResults"));
+            } catch (URISyntaxException | IOException e) {
+                e.printStackTrace();
+            }
         });
     }
 }
