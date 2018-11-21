7ca4941257c725deb699faa5e139022da0f9a17f
maciejprogramuje <maciejprogramuje@gmail.com>
2018-11-21 15:55:41 +0100

    ok

---
+++ src/main/java/pl/maciejprogramuje/webhose/managers/CopyToClipboard.java
@@ -0,0 +1,13 @@
+package pl.maciejprogramuje.webhose.managers;
+
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+
+public class CopyToClipboard {
+    public void copy(String string) {
+        final Clipboard clipboard = Clipboard.getSystemClipboard();
+        final ClipboardContent content = new ClipboardContent();
+        content.putString(string);
+        clipboard.setContent(content);
+    }
+}
